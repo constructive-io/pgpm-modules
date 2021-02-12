8a672406c3942ec52e20afe3536783dbdf7ade19
Dan Lynch <pyramation@gmail.com>
2021-02-11 23:14:49 -0700

    perms

--- packages/db_meta_modules/deploy/schemas/meta_public/tables/permissions_module/table.sql
+++ packages/db_meta_modules/deploy/schemas/meta_public/tables/permissions_module/table.sql
@@ -5,12 +5,33 @@
 BEGIN;
 
 CREATE TABLE meta_public.permissions_module (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 ()
-  -- fields
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    database_id uuid NOT NULL,
+    --
+    schema_id uuid NOT NULL DEFAULT uuid_nil(),
+    table_id uuid NOT NULL DEFAULT uuid_nil(),
+    table_name text NOT NULL DEFAULT 'permissions',
+    -- 
+
+    --
+    type_table_id uuid NOT NULL DEFAULT uuid_nil(),
+    type_table_name text NOT NULL DEFAULT 'permission_types',
+    -- 
+     
+    CONSTRAINT db_fkey FOREIGN KEY (database_id) REFERENCES collections_public.database (id) ON DELETE CASCADE,
+    CONSTRAINT schema_fkey FOREIGN KEY (schema_id) REFERENCES collections_public.schema (id) ON DELETE CASCADE,
+    CONSTRAINT table_fkey FOREIGN KEY (table_id) REFERENCES collections_public.table (id) ON DELETE CASCADE,
+    CONSTRAINT type_table_fkey FOREIGN KEY (type_table_id) REFERENCES collections_public.table (id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE meta_public.permissions_module IS 'A comment.';
+COMMENT ON CONSTRAINT schema_fkey ON meta_public.permissions_module IS E'@omit manyToMany';
+COMMENT ON CONSTRAINT db_fkey ON meta_public.permissions_module IS E'@omit manyToMany';
+CREATE INDEX permissions_module_database_id_idx ON meta_public.permissions_module ( database_id );
 
-COMMENT ON COLUMN meta_public.permissions_module.id IS 'The primary unique identifier for the permissions_module table.';
+COMMENT ON CONSTRAINT table_fkey
+     ON meta_public.permissions_module IS E'@omit manyToMany';
+
+COMMENT ON CONSTRAINT type_table_fkey
+     ON meta_public.permissions_module IS E'@omit manyToMany';
 
 COMMIT;
