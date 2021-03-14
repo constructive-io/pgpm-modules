484b83963112573a37f7b3747cae816da8dea561
Dan Lynch <pyramation@gmail.com>
2021-03-13 20:32:46 -0700

    pkg

--- packages/db_meta/sql/db_meta--0.2.48.sql
+++ packages/db_meta/sql/db_meta--0.2.48.sql
@@ -238,6 +238,7 @@ CREATE TABLE collections_public.policy (
 	permissive boolean DEFAULT ( TRUE ),
 	policy_template_name text,
 	policy_template_vars jsonb,
+	disabled boolean DEFAULT ( FALSE ),
 	CONSTRAINT db_fkey FOREIGN KEY ( database_id ) REFERENCES collections_public.database ( id ) ON DELETE CASCADE,
 	CONSTRAINT table_fkey FOREIGN KEY ( table_id ) REFERENCES collections_public."table" ( id ) ON DELETE CASCADE,
 	UNIQUE ( table_id, name ) 
