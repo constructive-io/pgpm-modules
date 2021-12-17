a684c409b62f293438f36720ebdc60b563bc5164
Dan Lynch <pyramation@gmail.com>
2021-12-16 18:18:51 -0800

    order

--- packages/db_meta_modules/deploy/schemas/meta_public/tables/denormalized_table_field/table.sql
+++ packages/db_meta_modules/deploy/schemas/meta_public/tables/denormalized_table_field/table.sql
@@ -21,6 +21,7 @@ CREATE TABLE meta_public.denormalized_table_field (
     update_defaults bool NOT NULL DEFAULT TRUE, 
 
     func_name text NULL,
+    func_order text NULL,
 
     --
     CONSTRAINT db_fkey FOREIGN KEY (database_id) REFERENCES collections_public.database (id) ON DELETE CASCADE,
