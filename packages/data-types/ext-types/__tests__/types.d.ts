9f8c263a211a8fe9b188df86911282152561b5b3
Dan Lynch <pyramation@gmail.com>
2025-08-15 02:10:43 +0000

    test(types,snaps): add missing local type decls and snapshots for restored tests
    Co-Authored-By: Dan Lynch <pyramation@gmail.com>

---
+++ packages/data-types/ext-types/__tests__/types.d.ts
@@ -0,0 +1,1 @@
+declare module '@launchql/db-testing';
---
+++ types/globals.d.ts
@@ -0,0 +1,5 @@
+declare module 'graphile-test';
+declare module '@launchql/db-testing';
+declare module '@launchql/graphql-testing';
+declare module 'pgsql-test';
+declare module 'jest-in-case';
